f7c97843460a0a6c034bda1552dfa5299bd43a76
Uxía Mesías <74102860+uxiamesias@users.noreply.github.com>
2022-01-08 18:36:29 +0100

    Update codigo.js

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -2,7 +2,7 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/twitter-logo-3'){
+  if (mySrc === 'http://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png'){
     myImage.setAttribute ('src', 'https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-3.png');
   } else {
     myImage.setAttribute ('src', 'http://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png');
